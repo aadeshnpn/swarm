e426de5ee2cd52688194e055e3de045b905b2186
Aadesh Neupane <neupaneaadesh@gmail.com>
2018-01-05 15:46:12 -0700

    changes to the bash script

--- batch_test.bash
+++ batch_test.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ~/anaconda2/bin/conda-env remove --name travis -y
-~/anaconda2/bin/conda create --name travis python=3.5
+~/anaconda2/bin/conda create --name travis python=3.5 -y
 
 source ~/anaconda2/bin/activate travis 
 
 pip install -r requirements.txt
 
-pip install coveralls, flake8
+pip install coveralls flake8
 
 pip install .
 
