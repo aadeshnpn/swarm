373efab5b931dbcf086683f3dec3c71a29e3edc3
Aadesh Neupane <neupaneaadesh@gmail.com>
2018-01-05 15:40:49 -0700

    setup files added

---
+++ batch_test.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+~/anaconda2/bin/conda-env remove --name travis -y
+~/anaconda2/bin/conda create --name travis python=3.5
+
+source ~/anaconda2/bin/activate travis 
+
+pip install -r requirements.txt
+
+pip install coveralls, flake8
+
+pip install .
+
+#flake8 
+
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+
+from setuptools import setup, find_packages
+from codecs import open
+
+requires = [
+    'numpy'
+]
+
+version = ''
+with open('__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+#with open('README.rst', 'rb', encoding='utf-8') as f:
+#    readme = f.read()
+
+setup(
+    name='swarm',
+    version=version,
+    description="Swarm modeling (ABM) in Python 3+",
+    long_description="testing",
+    author='Project Swarm Team',
+    author_email='aadeshnpn@byu.net',
+    url='https://github.com/aadeshnpn/swarm',
+    packages=find_packages(),
+    #package_data={'mesa': ['visualization/templates/*.html', 'visualization/templates/css/*',
+    #                       'visualization/templates/fonts/*', 'visualization/templates/js/*']},
+    #include_package_data=True,
+    install_requires=requires,
+    keywords='multi-agent',
+    license='Apache 2.0',
+    zip_safe=False,
+    classifiers=[
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Life',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+    ],
+)
