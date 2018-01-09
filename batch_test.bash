f7afce05db891b4b7b18c27043e08a2074858634
Aadesh Neupane <neupaneaadesh@gmail.com>
2018-01-08 19:41:13 -0700

    nose package added

--- batch_test.bash
+++ batch_test.bash
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-#~/anaconda2/bin/conda-env remove --name travis -y
-#~/anaconda2/bin/conda create --name travis python=3.5 -y
+~/anaconda2/bin/conda-env remove --name travis -y
+~/anaconda2/bin/conda create --name travis python=3.5 -y
 
 source ~/anaconda2/bin/activate travis 
 
-#pip install -r requirements.txt
+python --version
+pip install -r requirements.txt
 
-#pip install coveralls flake8
+pip install coveralls flake8 nose
 
-pip uninstall swarm -y
+#pip uninstall swarm -y
+
 pip install .
 
-#flake8 . --ignore=F403,E501,E123,E128 --exclude=docs,build
+flake8 . --ignore=F403,E501,E123,E128 --exclude=docs,build
 
-#nosetests --with-coverage --cover-package=swarm
+nosetests --with-coverage --cover-package=swarm
