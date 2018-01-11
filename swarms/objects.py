b39655462fa7356b7b04134023cc58cc05070cde
Aadesh Neupane <neupaneaadesh@gmail.com>
2018-01-11 10:05:27 -0700

    mode environment objects with good design principle

---
+++ swarms/objects.py
@@ -0,0 +1,136 @@
+# Base class for all the objects that can be defined in the environment
+class EnvironmentObject:
+    def __init__(self, id=1, location=(0, 0), radius=20, grid=[1]):
+        self.id = id
+        self.location = location
+        self.radius = radius
+        self.grid = grid
+
+
+# Class to define hub object
+class Hub(EnvironmentObject):
+    def __init__(self, id=1, location=(0, 0), radius=20, grid=[1]):
+        super().__init__(id, location, radius, grid)
+        self.carryable = False
+        self.dropable = True
+
+
+# Class to define site object
+class Sites(EnvironmentObject):
+    def __init__(self, id=1, location=(0, 0), radius=20, q_value=0.5, grid=[1]):
+        super().__init__(id, location, radius, grid)
+        self.q_value = q_value
+        self.food_unit = self.q_value * 1000
+        self.carryable = False
+
+
+# Class to define obstacle
+class Obstacles(EnvironmentObject):
+    def __init__(self, id=1, location=(0, 0), radius=20, grid=[1]):
+        super().__init__(id, location, radius, grid)
+        self.potential_field = None
+        self.carrable = True
+        self.dropable = True
+
+
+# Class to define communication
+class Communication(EnvironmentObject):
+    def __init__(self, id=1, location=(0, 0), radius=20, grid=[1], object_to_communicate=None):
+        super().__init__(id, location, radius, grid)
+        # Communication parameters for signal
+        self.communicated_object = object_to_communicate
+        self.communicated_location = self.communicated_object.location
+
+
+# Class to define signal
+class Signal(Communication):
+    def __init__(self, id=1, location=(0, 0), radius=20, grid=[1], object_to_communicate=None):
+        super().__init__(id, None, radius, None, object_to_communicate)
+
+
+# Class to define Cue
+class Cue(Communication):
+    def __init__(self, id=1, location=(0, 0), radius=20, grid=[1], object_to_communicate=None):
+        super().__init__(id, location, radius, grid, object_to_communicate)
+
+
+# Class to define Traps
+class Traps(EnvironmentObject):
+    def __init__(self, id=1, location=(0, 0), radius=20, grid=[1]):
+        super().__init__(id, location, radius, grid)
+        self.carryable = False
+
+
+# Class to define Food
+class Food:
+    def __init__(self, id=1, location=(0, 0), radius=2, grid=[1]):
+        super().__init__(id, location, radius, grid)
+        self.carryable = True
+
+
+# Class to define Derbis
+class Derbis:
+    def __init__(self, id=1, location=(0, 0), radius=2, grid=[1], weight=5):
+        super().__init__(id, location, radius, grid)        
+        self.carryable = True
+        self.weight = int(self.radius / 2)
+        self.shareholders = 0
+        self.motion = False
+        self.agentid = []
+
+
+# Class to define Rules
+class Rules:
+    def __init__(self, id=1, json_data=None):
+        self.id = id
+        # self.currstate = Cu(id,json_data[0]['currstate'])
+        self.currstate = Currstate(id, json_data[0]['currstate'])
+        self.luggage = Luggage(id, json_data[1]['luggage'])
+        self.movement = Movement(id, json_data[2]['movement'])
+        self.communicate = None
+
+
+# Class to define current state
+class Currstate:
+    def __init__(self, id=id, state_data=None):
+        self.currstate_data = state_data
+        self.curr_carry = self.currstate_data['carry']
+        self.curr_drop = self.currstate_data['drop']
+        self.curr_move = self.currstate_data['move']
+
+
+# Class to define Luggage
+class Luggage:
+    def __init__(self, id=id, luggage_data=None):
+        self.luggage_data = luggage_data
+        self.carry = Carry(id, self.luggage_data['carry'])
+        self.drop = Drop(id, self.luggage_data['drop'])
+
+
+# Class to define Movement
+class Movement:
+    def __init__(self, id=id, movement_data=None):
+        self.movement_data = movement_data
+        self.conditions = self.movement_data['conditions']
+        self.move = self.movement_data['move']
+        self.orientation = self.movement_data['orientation']
+
+        # self.carry = Carry(id,self.movement_data['carry'])
+        # self.drop = Drop(id,self.movement_data['drop'])
+        # print (self.conditions,self.move,self.orientation)
+
+
+# Class to define Carry
+class Carry:
+    def __init__(self, id=id, carry_data=None):
+        self.carry_data = carry_data
+        self.conditions = self.carry_data[0]
+        self.action = self.carry_data[1]
+
+
+# Class to define Drop
+class Drop:
+    def __init__(self, id=id, drop_data=None):
+        self.drop_data = drop_data
+        self.conditions = self.drop_data[0]
+        self.action = self.drop_data[1]
