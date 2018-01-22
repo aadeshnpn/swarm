798df05d86bcc1599bbec61cf880373e3a22dfde
Aadesh Neupane <neupaneaadesh@gmail.com>
2018-01-22 14:47:33 -0700

    test case for GE agent

--- test/test_wealth_behavior.py
+++ test/test_wealth_behavior.py
@@ -110,6 +110,99 @@ class Move(Behaviour):
     #    pass
 
 
+class GEAgent(Agent):
+    """ An minimalistic GE agent """
+    def __init__(self, name, model):
+        super().__init__(name, model)
+        self.location = ()
+
+        self.direction = model.random.rand() * (2 * np.pi)
+        self.speed = 2
+        self.radius = 3
+        
+        # self.exchange_time = model.random.randint(2, 4)
+        # This doesn't help. Maybe only perform genetic operations when 
+        # an agents meet 10% of its total population
+        # """
+        self.operation_threshold = 10
+        self.genome_storage = []
+
+        # Grammatical Evolution part
+        from ponyge.algorithm.parameters import Parameters
+        parameter = Parameters()
+        # list_params_files = ['string_match.txt', 'regression.txt', 'classification.txt']
+        parameter_list = ['--parameters', 'string_match_dist.txt']
+        parameter.params['RANDOM_SEED'] = 1234 #np.random.randint(1, 99999999)        
+        parameter.params['POPULATION_SIZE'] = self.operation_threshold // 2         
+        parameter.set_params(parameter_list)
+        self.parameter = parameter
+        individual = initialisation(self.parameter, 1)
+        individual = evaluate_fitness(individual, self.parameter)
+        self.individual = individual
+
+    def step(self):
+        # """
+        # Doing this is equivalent of using behavior tree with four classes
+        # in this order, Move, HasMoney, NeighbourCondition, ShareMoney
+        self.move()
+
+        cellmates = self.model.grid.get_objects_from_grid('GEAgent', self.location)
+
+        if len(self.genome_storage) >= self.operation_threshold:
+            self.exchange_chromosome(cellmates)
+
+        if len(cellmates) > 1:
+            self.store_genome(cellmates)
+
+    def advance(self):
+        pass
+
+    def move(self):
+        new_location = ()
+        x = int(self.location[0] + np.cos(self.direction) * self.speed)
+        y = int(self.location[1] + np.sin(self.direction) * self.speed)
+        new_location, direction = self.model.grid.check_limits((x, y), self.direction)
+        self.model.grid.move_object(self.location, self, new_location)
+        self.location = new_location
+        self.direction = direction
+
+    def store_genome(self, cellmates):
+        # cellmates.remove(self)
+        self.genome_storage += [agent.individual[0] for agent in cellmates]
+
+    def exchange_chromosome(self, cellmates):
+        individuals = self.genome_storage
+        parents = selection(self.parameter, individuals)
+        cross_pop = crossover(self.parameter, parents)
+        new_pop = mutation(self.parameter, cross_pop)
+        new_pop = evaluate_fitness(new_pop, self.parameter)
+        individuals = replacement(self.parameter, new_pop, individuals)
+        individuals.sort(reverse=True)
+        self.individual = [individuals[0]]
+        self.genome_storage = []    
+    
+class TestGESmallGrid(TestCase):
+    
+    def setUp(self):
+        self.environment = GEEnvironmentModel(100, 100, 100, 10, 123)
+
+        for i in range(200):
+            self.environment.step()
+
+        self.one_target = False
+        for agent in self.environment.schedule.agents:
+            self.target = agent.individual[0].phenotype
+
+            if agent.individual[0].phenotype == 'Hello':
+                self.one_target = True
+
+    def test_target_string(self):
+        self.assertEqual(self.target, 'Hello')
+
+    def test_one_traget(self):
+        self.assertEqual(self.one_target, True)
+
+
 class SwarmAgent(Agent):
     """ An minimalistic swarm agent """
     def __init__(self, name, model):
@@ -144,23 +237,6 @@ class SwarmAgent(Agent):
         # Based on research, use XML file to generate BT. Parse the XML BT
         # To actually get BT python program gm
 
-        # Grammatical Evolution part
-        from ponyge.algorithm.parameters import Parameters
-        parameter = Parameters()
-        # list_params_files = ['string_match.txt', 'regression.txt', 'classification.txt']
-        parameter_list = ['--parameters', 'string_match_dist.txt']
-        parameter.params['RANDOM_SEED'] = np.random.randint(1, 99999999)        
-        parameter.params['POPULATION_SIZE'] = self.operation_threshold // 2         
-        parameter.set_params(parameter_list)
-        self.parameter = parameter
-        individual = initialisation(self.parameter, 1)
-        individual = evaluate_fitness(individual, self.parameter)
-        self.individual = individual
-        # self.parameter.stats.get_stats(individual)
-        # individuals = parameter.params['SEARCH_LOOP'](parameter)
-        # parameter.stats.get_stats(individuals, end=True)
-        # get_stats(individuals, end=True)
-
     def step(self):
         # """
         # Doing this is equivalent of using behavior tree with four classes
@@ -168,12 +244,6 @@ class SwarmAgent(Agent):
         self.move()
 
         cellmates = self.model.grid.get_objects_from_grid('SwarmAgent', self.location)
-
-        if len(self.genome_storage) >= self.operation_threshold:
-            self.exchange_chromosome(cellmates)
-
-        if len(cellmates) > 1:
-            self.store_genome(cellmates)
 
         if self.wealth > 0:
             self.give_money(cellmates)
@@ -198,29 +268,14 @@ class SwarmAgent(Agent):
             other.wealth += 1
             self.wealth -= 1
 
-    def store_genome(self, cellmates):
-        cellmates.remove(self)
-        self.genome_storage += [agent.individual[0] for agent in cellmates]
 
-    def exchange_chromosome(self, cellmates):
-        individuals = self.genome_storage
-        parents = selection(self.parameter, individuals)
-        cross_pop = crossover(self.parameter, parents)
-        new_pop = mutation(self.parameter, cross_pop)
-        new_pop = evaluate_fitness(new_pop, self.parameter)
-        individuals = replacement(self.parameter, new_pop, individuals)
-        individuals.sort(reverse=True)
-        self.individual = [individuals[0]]
-        self.genome_storage = []
-
-
-class EnvironmentModel(Model):
+class WealthEnvironmentModel(Model):
     """ A environemnt to model swarms """
     def __init__(self, N, width, height, grid=10, seed=None):
         if seed is None:
-            super(EnvironmentModel, self).__init__(seed=None)
+            super(WealthEnvironmentModel, self).__init__(seed=None)
         else:
-            super(EnvironmentModel, self).__init__(seed)
+            super(WealthEnvironmentModel, self).__init__(seed)
 
         self.num_agents = N
 
@@ -238,6 +293,35 @@ class EnvironmentModel(Model):
             a.location = (x, y)
             self.grid.add_object_to_grid((x, y), a)
             a.operation_threshold = self.num_agents // 10
+
+    def step(self):
+        self.schedule.step()
+
+
+class GEEnvironmentModel(Model):
+    """ A environemnt to model swarms """
+    def __init__(self, N, width, height, grid=10, seed=None):
+        if seed is None:
+            super(GEEnvironmentModel, self).__init__(seed=None)
+        else:
+            super(GEEnvironmentModel, self).__init__(seed)
+
+        self.num_agents = N
+
+        self.grid = Grid(width, height, grid)
+
+        self.schedule = SimultaneousActivation(self)
+
+        for i in range(self.num_agents):
+            a = GEAgent(i, self)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = self.random.randint(-self.grid.width / 2, self.grid.width / 2)
+            y = self.random.randint(-self.grid.height / 2, self.grid.height / 2)
+
+            a.location = (x, y)
+            self.grid.add_object_to_grid((x, y), a)
+            a.operation_threshold = self.num_agents // 10
         # exit()
 
     def step(self):
@@ -245,11 +329,11 @@ class EnvironmentModel(Model):
 
 
 class TestWealthSwarmSmallGrid(TestCase):
-
+    
     def setUp(self):
-        self.environment = EnvironmentModel(100, 100, 100, 10, 123)
+        self.environment = WealthEnvironmentModel(100, 100, 100, 10, 123)
 
-        for i in range(300):
+        for i in range(50):
             self.environment.step()
 
         self.max_wealth = 0
@@ -259,24 +343,12 @@ class TestWealthSwarmSmallGrid(TestCase):
             if agent.wealth > self.max_wealth:
                 self.max_wealth = agent.wealth
                 self.max_agent = agent.name
-                self.target = agent.individual[0].phenotype
 
-            print(agent.name, agent.individual[0])
-            if agent.individual[0].phenotype == 'Hello World':
-                self.one_target = True
-    """
     def test_maximum_wealth(self):
         self.assertEqual(self.max_wealth, 6)
 
     def test_maximum_wealth_agent(self):
         self.assertEqual(self.max_agent, 75)
-
-    def test_target_string(self):
-        self.assertEqual(self.target, 'Hello')
-
-    """
-    def test_one_traget(self):
-        self.assertEqual(self.one_target, True)
 
 """
 class TestWealthSwarmBigGrid(TestCase):
