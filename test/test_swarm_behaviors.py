e0b8398939fe677b69be4c569bdf39d8fc65ea4d
Aadesh Neupane <neupaneaadesh@gmail.com>
2018-01-24 10:22:30 -0700

    test case for towards behavior

--- test/test_swarm_behaviors.py
+++ test/test_swarm_behaviors.py
@@ -5,7 +5,7 @@ from lib.time import SimultaneousActivation
 from lib.space import Grid
 from swarms.sbehaviors import (
     GoTo, RandomWalk,
-    Move, Away
+    Move, Away, Towards
     )
 from swarms.objects import Sites
 import py_trees
@@ -64,6 +64,34 @@ class SwarmAgentGoToAway(Agent):
 
     def advance(self):
         pass
+
+
+# Class to tets GoTo behavior with towards for agents
+class SwarmAgentGoToTowards(Agent):
+    """ An minimalistic behavior tree for swarm agent implementing goto away behavior"""
+    def __init__(self, name, model):
+        super().__init__(name, model)
+        self.location = ()
+
+        self.direction = model.random.rand() * (2 * np.pi)
+        self.speed = 2
+        self.radius = 3
+
+        root = py_trees.composites.Sequence("Sequence")
+        low = GoTo('1')
+        low.setup(0, self, model.target)
+        mid = Towards('2')
+        mid.setup(0, self)
+        high = Move('3')
+        high.setup(0, self)
+        root.add_children([low, mid, high])
+        self.behaviour_tree = py_trees.trees.BehaviourTree(root)
+
+    def step(self):
+        self.behaviour_tree.tick()
+
+    def advance(self):
+        pass        
 
 
 # class to test random walk behavior
@@ -155,6 +183,37 @@ class GoToAwaySwarmEnvironmentModel(Model):
         self.schedule.step()        
 
 
+class GoToTowardsSwarmEnvironmentModel(Model):
+    """ A environemnt to model swarms """
+    def __init__(self, N, width, height, grid=10, seed=None):
+        if seed is None:
+            super(GoToTowardsSwarmEnvironmentModel, self).__init__(seed=None)
+        else:
+            super(GoToTowardsSwarmEnvironmentModel, self).__init__(seed)
+
+        self.num_agents = N
+
+        self.grid = Grid(width, height, grid)
+
+        self.schedule = SimultaneousActivation(self)
+        
+        self.target = Sites(id=1, location=(45, 45), radius=5, q_value=0.5)
+
+        for i in range(self.num_agents):
+            a = SwarmAgentGoToTowards(i, self)
+            self.schedule.add(a)
+            x = 1
+            y = 11
+            a.location = (x, y)
+            a.direction = -2.3561944901923448
+            self.grid.add_object_to_grid((x, y), a)
+
+        self.agent = a
+
+    def step(self):
+        self.schedule.step()                
+
+
 class RandomWalkSwarmEnvironmentModel(Model):
     """ A environemnt to model swarms """
     def __init__(self, N, width, height, grid=10, seed=None):
@@ -208,7 +267,18 @@ class TestGoToAwaySwarmSmallGrid(TestCase):
 
     def test_agent_path(self):
         self.assertEqual(self.trimmed_results, [(20, 35), (18, 34), (-38, -18), (-39, -19)])
-        
+
+
+class TestGoToTowardsSwarmSmallGrid(TestCase):
+    
+    def setUp(self):
+        self.environment = GoToTowardsSwarmEnvironmentModel(1, 100, 100, 10, 123)
+        for i in range(50):
+            self.environment.step()
+
+    def test_agent_path(self):
+        self.assertEqual(self.environment.agent.location, (45, 45))
+
 
 class TestRandomWalkSwarmSmallGrid(TestCase):
     
