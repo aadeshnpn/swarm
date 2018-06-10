dde0ad82a769b1f2a26ebde0269013ced897d374
aadeshnpn <neupaneaadesh@gmail.com>
2018-06-10 11:40:38 -0600

    MoveAway composite behavior added

--- swarms/scbehaviors.py
+++ swarms/scbehaviors.py
@@ -13,7 +13,7 @@ from py_trees import Behaviour, Blackboard
 from py_trees.composites import Sequence
 from py_trees.trees import BehaviourTree
 from swarms.sbehaviors import (
-    GoTo, Towards, Move
+    GoTo, Towards, Move, Away
     )
 
 # Start of mid-level behaviors. These behaviors are the
@@ -64,6 +64,62 @@ class MoveTowards(Behaviour):
         # Define a sequence to combine the primitive behavior
         mt_sequence = Sequence('MT_SEQUENCE')
         mt_sequence.add_children([goto, towards, move])
+
+        self.behaviour_tree = BehaviourTree(mt_sequence)
+
+    def initialise(self):
+        """Everytime initialization. Not required for now."""
+        pass
+
+    def update(self):
+        """
+        Just call the tick method for the sequence.
+        This will execute the primitive behaviors defined in the sequence
+        """
+        self.behaviour_tree.tick()
+        return self.behaviour_tree.root.status
+
+
+class MoveAway(Behaviour):
+    """MoveAway behavior for the agents.
+
+    Inherits the Behaviors class from py_trees. This
+    behavior combines the privitive behaviors GoTo, Away and Move. This
+    allows agents actually to move away the object of interest.
+    """
+
+    def __init__(self, name):
+        """Init method for the MoveAway behavior."""
+        super(MoveAway, self).__init__(name)
+        self.blackboard = Blackboard()
+        self.blackboard.shared_content = dict()
+
+    def setup(self, timeout, agent, item):
+        """Have defined the setup method.
+
+        This method defines the other objects required for the
+        behavior. Agent is the actor in the environment,
+        item is the name of the item we are trying to find in the
+        environment and timeout defines the execution time for the
+        behavior.
+        """
+        self.agent = agent
+        self.item = item
+        # Define goto primitive behavior
+        goto = GoTo('MT_GOTO_1')
+        goto.setup(0, self.agent, self.item)
+
+        # Define away behavior
+        away = Away('MT_Away_2')
+        away.setup(0, self.agent)
+
+        # Define move behavior
+        move = Move('MT_MOVE_3')
+        move.setup(0, self.agent)
+
+        # Define a sequence to combine the primitive behavior
+        mt_sequence = Sequence('MT_SEQUENCE')
+        mt_sequence.add_children([goto, away, move])
 
         self.behaviour_tree = BehaviourTree(mt_sequence)
 
