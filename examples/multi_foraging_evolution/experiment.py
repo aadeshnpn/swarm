c3fd654e4890a5a37e37b1c25586933cfcde5c4d
Aadesh Neupane <neupaneaadesh@gmail.com>
2018-09-24 13:04:37 -0600

    code refactored for multi-source foraging problem

---
+++ examples/multi_foraging_evolution/experiment.py
@@ -0,0 +1,293 @@
+"""Experiment script to run Multi source foraging simulation."""
+
+from simmodel import SimModel
+from evolmodel import EvolModel
+
+# from swarms.utils.jsonhandler import JsonData
+from swarms.utils.graph import Graph, GraphACC
+from joblib import Parallel, delayed
+from swarms.utils.results import SimulationResults
+from swarms.utils.jsonhandler import JsonPhenotypeData
+# Global variables for width and height
+width = 100
+height = 100
+
+UI = False
+
+
+def extract_phenotype(agents, filename, method='ratio'):
+    """Extract phenotype of the learning agents.
+
+    Sort the agents based on the overall fitness and then based on the
+    method extract phenotype of the agents.
+    Method can take {'ratio','higest','sample'}
+    """
+    sorted_agents = sorted(
+        agents, key=lambda x: x.individual[0].fitness, reverse=True)
+
+    if method == 'ratio':
+        ratio_value = 0.4
+        upper_bound = ratio_value * len(agents)
+        selected_agents = agents[0:int(upper_bound)]
+        selected_phenotype = [
+            agent.individual[0].phenotype for agent in selected_agents]
+        # return selected_phenotype
+    else:
+        selected_phenotype = [sorted_agents[0].individual[0].phenotype]
+        # return [sorted_agents[0].individual[0].phenotype]
+
+    # Save the phenotype to a json file
+    JsonPhenotypeData.to_json(selected_phenotype, filename)
+
+    # Return the phenotype
+    return selected_phenotype
+
+
+def simulate_res1(env, iteration):
+    """Test the performane of evolved behavior."""
+    phenotypes = env[0]
+    threshold = 1.0
+
+    sim = SimModel(
+        100, 100, 100, 10, iter=iteration, xmlstrings=phenotypes, pname=env[1],
+        expname='MSFRes1', agent='SimAgentRes1')
+    sim.build_environment_from_json()
+
+    # for all agents store the information about hub
+    for agent in sim.agents:
+        agent.shared_content['Hub'] = {sim.hub}
+        # agent.shared_content['Sites'] = {sim.site}
+
+    simresults = SimulationResults(
+        sim.pname, sim.connect, sim.sn, sim.stepcnt, sim.food_in_hub(),
+        phenotypes[0]
+        )
+
+    simresults.save_phenotype()
+    simresults.save_to_file()
+
+    # Iterate and execute each step in the environment
+    for i in range(iteration):
+        # For every iteration we need to store the results
+        # Save them into db or a file
+        sim.step()
+        simresults = SimulationResults(
+            sim.pname, sim.connect, sim.sn, sim.stepcnt, sim.food_in_hub(),
+            phenotypes[0]
+            )
+        simresults.save_to_file()
+
+    # print ('food at site', len(sim.food_in_loc(sim.site.location)))
+    # print ('food at hub', len(sim.food_in_loc(sim.hub.location)))
+    # print("Total food in the hub", len(food_objects))
+
+    food_objects = sim.food_in_loc(sim.hub.location)
+
+    for food in food_objects:
+        print('simulate phenotye:', dir(food))
+    value = sim.food_in_hub()
+
+    foraging_percent = (
+        value * 100.0) / (sim.num_agents * 2.0)
+
+    sucess = False
+    print('Foraging percent', value)
+
+    if foraging_percent >= threshold:
+        print('Foraging success')
+        sucess = True
+
+    sim.experiment.update_experiment_simulation(value, sucess)
+
+    # Plot the fitness in the graph
+    graph = GraphACC(sim.pname, 'simulation.csv')
+    graph.gen_plot()
+
+
+def simulate_res2(env, iteration):
+    """Test the performane of evolved behavior."""
+    phenotypes = env[0]
+    threshold = 1.0
+
+    sim = SimModel(
+        100, 100, 100, 10, iter=iteration, xmlstrings=phenotypes, pname=env[1],
+        expname='MSFRes2', agent='SimAgentRes2')
+    sim.build_environment_from_json()
+
+    # for all agents store the information about hub
+    for agent in sim.agents:
+        agent.shared_content['Hub'] = {sim.hub}
+        # agent.shared_content['Sites'] = {sim.site}
+
+    simresults = SimulationResults(
+        sim.pname, sim.connect, sim.sn, sim.stepcnt, sim.food_in_hub(),
+        phenotypes[0]
+        )
+
+    simresults.save_phenotype()
+    simresults.save_to_file()
+
+    # Iterate and execute each step in the environment
+    for i in range(iteration):
+        # For every iteration we need to store the results
+        # Save them into db or a file
+        sim.step()
+        simresults = SimulationResults(
+            sim.pname, sim.connect, sim.sn, sim.stepcnt, sim.food_in_hub(),
+            phenotypes[0]
+            )
+        simresults.save_to_file()
+
+    # print ('food at site', len(sim.food_in_loc(sim.site.location)))
+    # print ('food at hub', len(sim.food_in_loc(sim.hub.location)))
+    # print("Total food in the hub", len(food_objects))
+
+    food_objects = sim.food_in_loc(sim.hub.location)
+
+    for food in food_objects:
+        print('simulate phenotye:', dir(food))
+    value = sim.food_in_hub()
+
+    foraging_percent = (
+        value * 100.0) / (sim.num_agents * 2.0)
+
+    sucess = False
+    print('Foraging percent', value)
+
+    if foraging_percent >= threshold:
+        print('Foraging success')
+        sucess = True
+
+    sim.experiment.update_experiment_simulation(value, sucess)
+
+    # Plot the fitness in the graph
+    graph = GraphACC(sim.pname, 'simulation.csv')
+    graph.gen_plot()
+
+
+def simulate(env, iteration):
+    """Test the performane of evolved behavior."""
+    # phenotype = agent.individual[0].phenotype
+    # phenotypes = extract_phenotype(agents)
+    phenotypes = env[0]
+    threshold = 1.0
+
+    sim = SimModel(
+        100, 100, 100, 10, iter=iteration, xmlstrings=phenotypes, pname=env[1])
+    sim.build_environment_from_json()
+
+    # for all agents store the information about hub
+    for agent in sim.agents:
+        agent.shared_content['Hub'] = {sim.hub}
+        # agent.shared_content['Sites'] = {sim.site}
+
+    simresults = SimulationResults(
+        sim.pname, sim.connect, sim.sn, sim.stepcnt, sim.food_in_hub(),
+        phenotypes[0]
+        )
+
+    simresults.save_phenotype()
+    simresults.save_to_file()
+
+    # Iterate and execute each step in the environment
+    for i in range(iteration):
+        # For every iteration we need to store the results
+        # Save them into db or a file
+        sim.step()
+        simresults = SimulationResults(
+            sim.pname, sim.connect, sim.sn, sim.stepcnt, sim.food_in_hub(),
+            phenotypes[0]
+            )
+        simresults.save_to_file()
+
+    # print ('food at site', len(sim.food_in_loc(sim.site.location)))
+    # print ('food at hub', len(sim.food_in_loc(sim.hub.location)))
+    # print("Total food in the hub", len(food_objects))
+
+    food_objects = sim.food_in_loc(sim.hub.location)
+
+    for food in food_objects:
+        print('simulate phenotye:', dir(food))
+    value = sim.food_in_hub()
+
+    foraging_percent = (
+        value * 100.0) / (sim.num_agents * 2.0)
+
+    sucess = False
+    print('Foraging percent', value)
+
+    if foraging_percent >= threshold:
+        print('Foraging success')
+        sucess = True
+
+    sim.experiment.update_experiment_simulation(value, sucess)
+
+    # Plot the fitness in the graph
+    graph = GraphACC(sim.pname, 'simulation.csv')
+    graph.gen_plot()
+
+
+def evolve(iteration):
+    """Learning Algorithm block."""
+    # iteration = 10000
+
+    env = EvolModel(100, 100, 100, 10, iter=iteration)
+    env.build_environment_from_json()
+
+    # for all agents store the information about hub
+    for agent in env.agents:
+        agent.shared_content['Hub'] = {env.hub}
+
+    # Hub and site object
+    # print(env.hub, env.site)
+
+    # Iterate and execute each step in the environment
+    for i in range(iteration):
+        env.step()
+
+    env.experiment.update_experiment()
+
+    # Collecting phenotypes on the basis of food collected
+    # Find if food has been deposited in the hub
+    food_objects = env.food_in_loc(env.hub.location)
+    # print('Total food in the hub evolution:', len(food_objects))
+    env.phenotypes = []
+    for food in food_objects:
+        print(food.phenotype)
+        env.phenotypes += list(food.phenotype.values())
+
+    jfilename = env.pname + '/' + env.runid + '.json'
+    JsonPhenotypeData.to_json(env.phenotypes, jfilename)
+
+    # Not using this method right now
+    # env.phenotypes = extract_phenotype(env.agents, jfilename)
+
+    # Plot the fitness in the graph
+    graph = Graph(env.pname, 'best.csv', ['explore', 'foraging'])
+    graph.gen_best_plots()
+
+    # Test the evolved behavior
+    return env
+
+
+def main(iter):
+    """Block for the main function."""
+    print('=======Start=========')
+    env = evolve(iter)
+    # simulate(None, iter)
+    if len(env.phenotypes) > 1:
+        steps = [5000 for i in range(8)]
+        env = (env.phenotypes, env.pname)
+        Parallel(n_jobs=4)(delayed(simulate)(env, i) for i in steps)
+        Parallel(n_jobs=4)(delayed(simulate_res1)(env, i) for i in steps)
+        Parallel(n_jobs=4)(delayed(simulate_res2)(env, i) for i in steps)
+        # simulate(env, 10000)
+    print('=======End=========')
+
+
+if __name__ == '__main__':
+    # Running 50 experiments in parallel
+    # steps = [100000 for i in range(50)]
+    # Parallel(n_jobs=8)(delayed(main)(i) for i in steps)
+    # Parallel(n_jobs=4)(delayed(main)(i) for i in range(1000, 100000, 2000))
+    main(900)
---
+++ examples/multi_foraging_evolution/simmodel.py
@@ -0,0 +1,204 @@
+"""Inherited model class."""
+
+from swarms.lib.model import Model
+from swarms.lib.time import SimultaneousActivation
+from swarms.lib.space import Grid
+from swarms.utils.jsonhandler import JsonData
+from swarms.utils.results import Best, Experiment   # noqa: F401
+from swarms.utils.db import Connect
+from simagent import (  # noqa: F401
+    SimAgent, SimAgentRes1, SimAgentRes2,
+    SimAgentResComm1, SimAgentResComm2)
+from swarms.lib.objects import Hub, Sites, Food, Derbis, Obstacles  # noqa: F401
+import os
+import imp
+import datetime
+import numpy as np
+from swarms.utils.ui import UI
+
+filename = os.path.join(imp.find_module("swarms")[1] + "/utils/world.json")
+
+
+class SimModel(Model):
+    """A environemnt to model swarms."""
+
+    def __init__(
+            self, N, width, height, grid=10, iter=100000,
+            xmlstrings=None, seed=None, viewer=False, pname=None,
+            expname='MSFSimulation', agent='SimAgent'):
+        """Initialize the attributes."""
+        if seed is None:
+            super(SimModel, self).__init__(seed=None)
+        else:
+            super(SimModel, self).__init__(seed)
+
+        self.runid = datetime.datetime.now().strftime(
+            "%s") + str(self.random.randint(1, 1000, 1)[0])
+
+        if pname is None:
+            self.pname = os.getcwd() + '/' + self.runid + expname
+        else:
+            self.pname = pname + '/' + self.runid + expname
+
+        self.width = width
+        self.height = height
+        self.stepcnt = 1
+        self.iter = iter
+        self.xmlstrings = xmlstrings
+
+        self.viewer = viewer
+
+        # Create db connection
+        connect = Connect('swarm', 'swarm', 'swarm', 'localhost')
+        self.connect = connect.tns_connect()
+
+        # Fill out the experiment table
+        self.experiment = Experiment(
+            self.connect, self.runid, N, seed, expname,
+            iter, width, height, grid, phenotype=xmlstrings[0])
+
+        self.experiment.insert_experiment_simulation()
+
+        self.sn = self.experiment.sn
+
+        # Create a folder to store results
+        os.mkdir(self.pname)
+
+        self.num_agents = N
+
+        self.grid = Grid(width, height, grid)
+
+        self.schedule = SimultaneousActivation(self)
+
+        self.agents = []
+
+        bound = np.ceil((self.num_agents * 1.0) / len(self.xmlstrings))
+
+        j = 0
+        # Create agents
+        for i in range(self.num_agents):
+            # print (i, j, self.xmlstrings[j])
+            a = eval(agent)(i, self, xmlstring=self.xmlstrings[j])
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            # x = self.random.randint(
+            #    -self.grid.width / 2, self.grid.width / 2)
+            x = 0
+            # y = self.random.randint(
+            #    -self.grid.height / 2, self.grid.height / 2)
+            y = 0
+
+            a.location = (x, y)
+            self.grid.add_object_to_grid((x, y), a)
+            a.operation_threshold = 2  # self.num_agents // 10
+            self.agents.append(a)
+
+            if (i + 1) % bound == 0:
+                j += 1
+
+    def create_environment_object(self, jsondata, obj):
+        """Create env from jsondata."""
+        name = obj.__name__.lower()
+        temp_list = []
+        i = 0
+        for json_object in jsondata[name]:
+            location = (json_object["x"], json_object["y"])
+            if "q_value" in json_object:
+                temp_obj = obj(
+                    i, location, json_object["radius"], q_value=json_object[
+                        "q_value"])
+            else:
+                temp_obj = obj(i, location, json_object["radius"])
+
+            self.grid.add_object_to_grid(location, temp_obj)
+            temp_list.append(temp_obj)
+            i += 1
+        return temp_list
+
+    def build_environment_from_json(self):
+        """Build env from jsondata."""
+        jsondata = JsonData.load_json_file(filename)
+        # Create a instance of JsonData to store object that
+        # needs to be sent to UI
+        self.render = JsonData()
+        self.render.objects = {}
+
+        for name in jsondata.keys():
+            obj = eval(name.capitalize())
+            self.render.objects[name] = self.create_environment_object(
+                jsondata, obj)
+
+        self.hub = self.render.objects['hub'][0]
+        try:
+            self.foods = []
+            for site in self.render.objects['sites']:
+                self.site = site  # self.render.objects['sites'][0]
+
+                for i in range(self.num_agents):
+                    f = Food(
+                        i, location=self.site.location,
+                        radius=self.site.radius)
+                    f.agent_name = None
+                    self.grid.add_object_to_grid(f.location, f)
+                    self.foods.append(f)
+        except KeyError:
+            pass
+
+        if self.viewer:
+            self.ui = UI(
+                (self.width, self.height), [self.hub], self.agents,
+                [self.site], food=self.foods)
+
+    def step(self):
+        """Step through the environment."""
+        # Gather info from all the agents
+        # self.gather_info()
+        # Next step
+        self.schedule.step()
+        # Increment the step count
+        self.stepcnt += 1
+        if self.viewer:
+            self.ui.step()
+
+    def find_higest_performer(self):
+        """Find the best agent."""
+        fitness = self.agents[0].individual[0].fitness
+        fittest = self.agents[0]
+        for agent in self.agents:
+            if agent.individual[0].fitness > fitness:
+                fittest = agent
+        return fittest
+
+    def find_higest_food_collector(self):
+        """Find the best agent to collect food."""
+        fitness = self.agents[0].food_collected
+        fittest = self.agents[0]
+        for agent in self.agents:
+            if agent.food_collected > fitness:
+                fittest = agent
+        return fittest
+
+    def detect_food_moved(self):
+        """Detect food moved."""
+        grid = self.grid
+        food_loc = self.site.location
+        neighbours = grid.get_neighborhood(food_loc, 10)
+        food_objects = grid.get_objects_from_list_of_grid('Food', neighbours)
+
+        # print (food_objects)
+        return food_objects
+
+    def food_in_hub(self):
+        """Find amount of food in hub."""
+        grid = self.grid
+        food_loc = self.hub.location
+        neighbours = grid.get_neighborhood(food_loc, 10)
+        food_objects = grid.get_objects_from_list_of_grid('Food', neighbours)
+        return len(food_objects)
+
+    def food_in_loc(self, loc):
+        """Find amount of food in hub."""
+        grid = self.grid
+        neighbours = grid.get_neighborhood(loc, 10)
+        food_objects = grid.get_objects_from_list_of_grid('Food', neighbours)
+        return food_objects
