f1292c2fab2075a4aab25b1ac78685660db81a73
aadeshnpn <neupaneaadesh@gmail.com>
2018-09-28 09:03:38 -0600

    changes to foraging experiment

--- examples/acc/experiment.py
+++ examples/acc/experiment.py
@@ -142,9 +142,9 @@ def simulate_res2(env, iteration):
 
     food_objects = sim.food_in_loc(sim.hub.location)
 
-    for food in food_objects:
-        print('simulate phenotye:', dir(food))
-    value = sim.food_in_hub()
+    # for food in food_objects:
+    #    print('simulate phenotye:', dir(food))
+    # value = sim.food_in_hub()
 
     foraging_percent = (
         value * 100.0) / (sim.num_agents * 2.0)
@@ -277,9 +277,11 @@ def main(iter):
     if len(env.phenotypes) > 1:
         steps = [5000 for i in range(8)]
         env = (env.phenotypes, env.pname)
-        Parallel(n_jobs=4)(delayed(simulate)(env, i) for i in steps)
-        Parallel(n_jobs=4)(delayed(simulate_res1)(env, i) for i in steps)
-        Parallel(n_jobs=4)(delayed(simulate_res2)(env, i) for i in steps)
+        #Parallel(n_jobs=8)(delayed(simulate)(env, i) for i in steps)
+        for i in steps:
+            simulate(env, i)
+        # Parallel(n_jobs=4)(delayed(simulate_res1)(env, i) for i in steps)
+        # Parallel(n_jobs=4)(delayed(simulate_res2)(env, i) for i in steps)
         # simulate(env, 10000)
     print('=======End=========')
 
@@ -288,5 +290,8 @@ if __name__ == '__main__':
     # Running 50 experiments in parallel
     # steps = [100000 for i in range(50)]
     # Parallel(n_jobs=8)(delayed(main)(i) for i in steps)
-    # Parallel(n_jobs=4)(delayed(main)(i) for i in range(1000, 100000, 2000))
-    main(90000)
+    Parallel(n_jobs=16)(delayed(main)(i) for i in range(10000, 1000000, 2000))
+    # main(90000)
+    #for i in range(10000, 1000000, 2000):
+    #    main(i)
+
