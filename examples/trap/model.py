be3eb6e3e46519dfc372510d9e5376671de2f4a5
aadeshnpn <neupaneaadesh@gmail.com>
2021-08-05 12:48:06 -0400

    mathematical issue solved with the avoid objects

--- examples/trap/model.py
+++ examples/trap/model.py
@@ -36,10 +36,10 @@ class SimTrapModel(Model):
         for i in range(self.num_agents):
             a = SwarmAgentAvoid(i, self)
             self.schedule.add(a)
-            x = -80 #-190 + self.random.randint(-10, 50)
-            y = -80 #-190 + self.random.randint(-10, 50)
+            x = -190 + self.random.randint(-10, 50)
+            y = -190 + self.random.randint(-10, 50)
             a.location = (x, y)
-            a.direction = -2.3561944901923448
+            a.direction = -2.3661944901923448
             self.grid.add_object_to_grid((x, y), a)
             self.agents.append(a)
 
