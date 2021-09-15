3a69ad14fcc2a77fb5b68225fd1ed692abada7ce
Aadesh <aadeshnpn@byu.edu>
2021-09-15 15:56:38 -0400

    a bash script added to run nestm experiment

---
+++ examples/nestm_evolution/experiments.sh
@@ -0,0 +1,4 @@
+python experiment.py --exp_no 0 --runs 24 --thread 6 --fitid 0
+python experiment.py --exp_no 0 --runs 24 --thread 6 --fitid 1
+python experiment.py --exp_no 0 --runs 24 --thread 6 --fitid 2
+python experiment.py --exp_no 0 --runs 24 --thread 6 --fitid 3
