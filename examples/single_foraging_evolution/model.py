5f1ea19bf96cf5c3c45bf13bb68a4f2e40d64a94
aadeshnpn <neupaneaadesh@gmail.com>
2021-09-17 19:15:07 -0400

    experiments script added to run bunch of experiment with old swarm behaviors

--- examples/single_foraging_evolution/model.py
+++ examples/single_foraging_evolution/model.py
@@ -7,10 +7,12 @@ from swarms.lib.space import Grid
 from swarms.utils.jsonhandler import JsonData
 from swarms.utils.results import Best, Experiment
 from swarms.utils.db import Connect
+from swarms.utils.ui import UI
 from agent import LearningAgent, ExecutingAgent, TestingAgent
 from swarms.lib.objects import (    # noqa : F401
-    Hub, Sites, Food, Debris, Obstacles)
+    Hub, Sites, Food, Debris, Obstacles, Traps)
 import os
+from pathlib import Path
 import imp
 import datetime
 import numpy as np
@@ -414,3 +416,314 @@ class TestModel(ForagingModel):
 
             if (i + 1) % bound == 0:
                 j += 1
+
+
+class SimForgModel(Model):
+    """A environemnt to model swarms."""
+
+    def __init__(
+            self, N, width, height, grid=10, iter=100000,
+            xmlstrings=None, seed=None, viewer=False, pname=None,
+            agent=ExecutingAgent, expsite=None, trap=5, obs=5, notrap=1, noobs=1, nosite=1):
+        """Initialize the attributes."""
+        if seed is None:
+            super(SimForgModel, self).__init__(seed=None)
+        else:
+            super(SimForgModel, self).__init__(seed)
+
+        self.width = width
+        self.height = height
+        self.stepcnt = 1
+        self.iter = iter
+        self.xmlstrings = xmlstrings
+
+        self.viewer = viewer
+        self.agent = agent
+        self.expsite = expsite
+        self.trap_radius = trap
+        self.obs_radius = obs
+        self.no_trap = notrap
+        self.no_obs = noobs
+        self.no_site = nosite
+        # print('agent type', agent)
+        # # Create db connection
+        # try:
+        #     connect = Connect('swarm', 'swarm', 'swarm', 'localhost')
+        #     self.connect = connect.tns_connect()
+        # except:
+        #     pass
+        self.connect = None
+        # # # Fill out the experiment table
+        # self.experiment = Experiment(
+        #     self.connect, self.runid, N, seed, 'Simuation Single Foraging',
+        #     iter, width, height, grid, phenotype=xmlstrings[0])
+
+        # self.experiment.insert_experiment_simulation()
+
+        # self.sn = self.experiment.sn
+        self.sn = 1
+        # Create a folder to store results
+        while True:
+            self.runid = datetime.datetime.now().strftime(
+                    "%s") + str(self.random.randint(1, 10000, 1)[0])
+            if pname is None:
+                self.pname = os.getcwd() + '/' + self.runid + "SForagingSimulationOld"
+            else:
+                self.pname = pname + '/' + self.runid + "SForagingSimulationOld"
+            if not Path(self.pname).exists():
+                Path(self.pname).mkdir(parents=True, exist_ok=False)
+                break
+
+        self.num_agents = N
+
+        self.grid = Grid(width, height, grid)
+
+        self.schedule = SimultaneousActivation(self)
+        self.traps = []
+        self.obstacles = []
+
+        self.agents = []
+
+        bound = np.ceil((self.num_agents * 1.0) / len(self.xmlstrings))
+
+        j = 0
+        # Create agents
+        for i in range(self.num_agents):
+            # print (i, j, self.xmlstrings[j])
+            a = self.agent(i, self, xmlstring=self.xmlstrings)
+            self.schedule.add(a)
+            # Add the hub to agents memory
+            # Add the hub to agents memory
+            # a.shared_content['Hub'] = {self.hub}
+            # Initialize the BT. Since the agents are normal agents just
+            # use the phenotype
+            a.construct_bt()
+            # Add the agent to a random grid cell
+            # x = self.random.randint(
+            #    -self.grid.width / 2, self.grid.width / 2)
+            x = 0
+            # y = self.random.randint(
+            #    -self.grid.height / 2, self.grid.height / 2)
+            y = 0
+
+            a.location = (x, y)
+            self.grid.add_object_to_grid((x, y), a)
+            a.operation_threshold = 2  # self.num_agents // 10
+            self.agents.append(a)
+
+            if (i + 1) % bound == 0:
+                j += 1
+
+        # Add equal number of food source
+        # for i in range(20):
+        #    f = Food(i, location=(-29, -29), radius=5)
+        #    self.grid.add_object_to_grid(f.location, f)
+            # print (i,x,y)
+
+    def place_site(self):
+        theta = np.linspace(0, 2*np.pi, 36)
+        while True:
+            t = self.random.choice(theta, 1, replace=False)[0]
+            x = int(self.hub.location[0] + np.cos(t) * self.expsite)
+            y = int(self.hub.location[0] + np.sin(t) * self.expsite)
+            location = (x, y)
+            radius = 10
+            q_value = 0.9
+            other_bojects = self.grid.get_objects_from_list_of_grid(None, self.grid.get_neighborhood((x,y), radius))
+            if len(other_bojects) == 0:
+                self.site = Sites(
+                        0, location, radius, q_value=q_value)
+                self.grid.add_object_to_grid(location, self.site)
+                break
+
+    def place_static_objs(self, obj, radius):
+        theta = np.linspace(0, 2*np.pi, 36)
+        while True:
+            dist = self.random.choice(range(25, self.width//2, 5))
+            t = self.random.choice(theta, 1, replace=False)[0]
+            x = int(0 + np.cos(t) * dist)
+            y = int(0 + np.sin(t) * dist)
+            location = (x, y)
+            other_bojects = self.grid.get_objects_from_list_of_grid(None, self.grid.get_neighborhood((x,y), radius))
+            # print(obj, radius, location)
+            if len(other_bojects) == 0:
+                envobj = obj(
+                        dist, location, radius)
+                self.grid.add_object_to_grid(location, envobj)
+                # bojects = self.grid.get_objects_from_list_of_grid('Traps', self.grid.get_neighborhood(location, radius))
+                # print('reverse', bojects)
+                if isinstance(envobj, Traps):
+                    self.traps += [envobj]
+                if isinstance(envobj, Obstacles):
+                    self.obstacles += [envobj]
+                break
+
+    def create_environment_object(self, jsondata, obj):
+        """Create env from jsondata."""
+        name = obj.__name__.lower()
+        temp_list = []
+        i = 0
+        for json_object in jsondata[name]:
+            location = (json_object["x"], json_object["y"])
+            if "q_value" in json_object:
+                temp_obj = None
+                pass
+                # temp_obj = obj(
+                #     i, location, json_object["radius"], q_value=json_object[
+                #         "q_value"])
+            else:
+                if name == 'traps':
+                    # temp_obj = obj(i, location, self.trap_radius)
+                    temp_obj = None
+                    for t in range(self.no_trap):
+                        self.place_static_objs(Traps, self.trap_radius)
+                elif name =='obstacles':
+                    temp_obj = None
+                    for o in range(self.no_obs):
+                        self.place_static_objs(Obstacles, self.obs_radius)
+                    # temp_obj = obj(i, location, self.obs_radius)
+                elif name == 'hub':
+                    temp_obj = obj(i, location, json_object["radius"])
+            if temp_obj is not None:
+                self.grid.add_object_to_grid(location, temp_obj)
+                temp_list.append(temp_obj)
+                i += 1
+        return temp_list
+
+    def build_environment_from_json(self):
+        """Build env from jsondata."""
+        jsondata = JsonData.load_json_file(filename)
+        # Create a instance of JsonData to store object that
+        # needs to be sent to UI
+        self.render = JsonData()
+        self.render.objects = {}
+
+        for name in jsondata.keys():
+            obj = eval(name.capitalize())
+            self.render.objects[name] = self.create_environment_object(
+                jsondata, obj)
+
+        self.hub = self.render.objects['hub'][0]
+        # self.obstacles = self.render.objects['obstacles'][0]
+        # print(self.obstacles.passable)
+        # self.traps = self.render.objects['traps'][0]
+        # add site with random distances
+        for s in range(self.no_site):
+            self.place_site()
+        # print(self.traps, self.obstacles)
+        # print(self.traps, self.obstacles, self.hub, self.site)
+        # location = (self.expsite["x"], self.expsite["y"])
+        # self.site = Sites(
+        #         0, location, self.expsite["radius"], q_value=self.expsite[
+        #             "q_value"])
+
+        # self.grid.add_object_to_grid(location, self.site)
+
+        try:
+            # self.site = self.render.objects['sites'][0]
+            self.foods = []
+            for i in range(self.num_agents * 1):
+                f = Food(
+                    i, location=self.site.location, radius=self.site.radius)
+                f.agent_name = None
+                self.grid.add_object_to_grid(f.location, f)
+                self.foods.append(f)
+        except KeyError:
+            pass
+
+        # Add trap
+        # try:
+        #     # x,y = self.site.location
+        #     # x = self.random.randint(x+20, x+40)
+        #     # y = self.random.randint(y+20, y+40)
+        #     # # print(self.site.location, x, y)
+        #     # self.traps = []
+        #     # f = Traps(
+        #     #     i, location=(x,y), radius=20)
+        #     # f.agent_name = None
+        #     # self.grid.add_object_to_grid(f.location, f)
+        #     # self.traps.append(f)
+        # except KeyError:
+        #     pass
+
+        if self.viewer:
+            self.ui = UI(
+                (self.width, self.height), [self.hub], self.agents,
+                [self.site], food=self.foods)
+
+    def step(self):
+        """Step through the environment."""
+        # Gather info from all the agents
+        # self.gather_info()
+        # Next step
+        self.schedule.step()
+        # Increment the step count
+        self.stepcnt += 1
+        if self.viewer:
+            self.ui.step()
+
+    def find_higest_performer(self):
+        """Find the best agent."""
+        fitness = self.agents[0].individual[0].fitness
+        fittest = self.agents[0]
+        for agent in self.agents:
+            if agent.individual[0].fitness > fitness:
+                fittest = agent
+        return fittest
+
+    def find_higest_food_collector(self):
+        """Find the best agent to collect food."""
+        fitness = self.agents[0].food_collected
+        fittest = self.agents[0]
+        for agent in self.agents:
+            if agent.food_collected > fitness:
+                fittest = agent
+        return fittest
+
+    def detect_food_moved(self):
+        """Detect food moved."""
+        grid = self.grid
+        food_loc = self.site.location
+        neighbours = grid.get_neighborhood(food_loc, 10)
+        food_objects = grid.get_objects_from_list_of_grid('Food', neighbours)
+
+        # print (food_objects)
+        return food_objects
+
+    def food_in_hub(self):
+        """Find amount of food in hub."""
+        # grid = self.grid
+        # food_loc = self.hub.location
+        # neighbours = grid.get_neighborhood(food_loc, self.hub.radius)
+        # food_objects = grid.get_objects_from_list_of_grid('Food', neighbours)
+        # return len(food_objects)
+        grid = self.grid
+        hub_loc = self.hub.location
+        neighbours = grid.get_neighborhood(hub_loc, self.hub.radius)
+        food_objects = grid.get_objects_from_list_of_grid('Food', neighbours)
+        _, hub_grid = grid.find_grid(hub_loc)
+        for food in self.foods:
+            _, food_grid = grid.find_grid(food.location)
+            if food_grid == hub_grid:
+                food_objects += [food]
+        food_objects = set(food_objects)
+        # total_food_weights = sum([food.weight for food in food_objects])
+        # return np.round(((total_food_weights * 1.0) / self.total_food_units) * 100, 1)
+        return len(food_objects)
+
+    def food_in_loc(self, loc):
+        """Find amount of food in hub."""
+        grid = self.grid
+        neighbours = grid.get_neighborhood(loc, 10)
+        food_objects = grid.get_objects_from_list_of_grid('Food', neighbours)
+        return food_objects
+
+    def no_agent_dead(self):
+        grid = self.grid
+        no_dead = 0
+        for trap in self.traps:
+            trap_loc = trap.location
+            neighbours = grid.get_neighborhood(trap_loc, 10)
+            agents = grid.get_objects_from_list_of_grid(type(self.agents[0]).__name__, neighbours)
+            no_dead += sum([1 if a.dead else 0 for a in agents])
+        return no_dead
