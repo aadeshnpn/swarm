667d749a0455acf70533b9fff5378f4839772ed5
aadeshnpn <neupaneaadesh@gmail.com>
2021-09-24 10:31:33 -0400

    grammar corrected

--- examples/nestm_evolution/experiments.sh
+++ examples/nestm_evolution/experiments.sh
@@ -1,6 +1,7 @@
 python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 0
-python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 0
-python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 1
 python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 1
 python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 3
-python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 3
+# python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 0
+# python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 1
+# python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 3
+
