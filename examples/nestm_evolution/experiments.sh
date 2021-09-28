914abace4230e863ed9c4604db2d0e00aafaf28b
aadeshnpn <neupaneaadesh@gmail.com>
2021-09-28 13:19:15 -0400

    paramater and grammar file added for coevolution experiments

--- examples/nestm_evolution/experiments.sh
+++ examples/nestm_evolution/experiments.sh
@@ -1,7 +1,12 @@
 python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 0
 python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 1
 python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 3
-# python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 0
-# python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 1
-# python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 3
-
+python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 0
+python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 1
+python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 3
+python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 0
+python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 1
+python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 3
+python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 0
+python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 1
+python experiment.py --exp_no 0 --iter 12000 --runs 16 --thread 16 --fitid 3
