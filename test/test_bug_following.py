c950f4e63740f4e0e06e2582f9c8c03612f99bc3
aadeshnpn <neupaneaadesh@gmail.com>
2022-02-01 20:48:47 -0500

    test case added for trap bug following algo

--- test/test_bug_following.py
+++ test/test_bug_following.py
@@ -111,3 +111,98 @@ class TestGoToSwarmSmallGrid(TestCase):
     def test_agent_grid(self):
         self.assertIsInstance(
             self.environment.grid.get_objects_from_grid('Sites',self.environment.agent.location)[0], Sites)
+
+
+
+
+# Class to tets Passable attribute for agents
+class SwarmAgentTrapGoTo(Agent):
+    """ An minimalistic behavior tree for swarm agent implementing goto
+    behavior
+    """
+    def __init__(self, name, model):
+        super().__init__(name, model)
+        self.location = ()
+
+        self.direction = model.random.rand() * (2 * np.pi)
+        self.speed = 2
+        self.radius = 5
+
+        self.moveable = True
+        self.shared_content = dict()
+
+        root = py_trees.composites.Sequence("Sequence")
+
+
+        self.shared_content = dict()
+        self.shared_content[type(model.target).__name__] = {model.target}
+
+        # Defining the composite behavior
+        movetowards = MoveTowards('MoveTowards')
+
+        # Setup for the behavior
+        movetowards.setup(0, self, 'Sites')
+
+        # This behavior is just defined to check if the composite tree
+        # can be combined with other primite behaviors
+        seq = Sequence('Seq')
+        seq.add_children([movetowards])
+
+        # Since its root is a sequence, we can use it directly
+        self.behaviour_tree = BehaviourTree(seq)
+
+    def step(self):
+        self.behaviour_tree.tick()
+
+
+class GoToSwarmEnvironmentTrapModel(Model):
+    """ A environemnt to model swarms """
+    def __init__(self, N, width, height, grid=10, seed=None):
+        if seed is None:
+            super(GoToSwarmEnvironmentTrapModel, self).__init__(seed=None)
+        else:
+            super(GoToSwarmEnvironmentTrapModel, self).__init__(seed)
+
+        self.num_agents = N
+
+        self.grid = Grid(width, height, grid)
+
+        self.schedule = SimultaneousActivation(self)
+
+        self.trap = Traps(id=2, location=(9, 9), radius=5)
+
+        self.target = Sites(id=1, location=(45, 45), radius=5, q_value=0.5)
+
+        self.grid.add_object_to_grid(self.target.location, self.target)
+        self.grid.add_object_to_grid(self.trap.location, self.trap)
+
+        for i in range(self.num_agents):
+            a = SwarmAgentTrapGoTo(i, self)
+            self.schedule.add(a)
+            x = -45
+            y = -30
+            a.location = (x, y)
+            a.direction = -2.3561944901923448
+            self.grid.add_object_to_grid((x, y), a)
+
+        self.agent = a
+
+    def step(self):
+        self.schedule.step()
+
+
+class TestGoToSwarmTrapSmallGrid(TestCase):
+
+    def setUp(self):
+        self.environment = GoToSwarmEnvironmentTrapModel(1, 100, 100, 10, 123)
+
+        for i in range(72):
+            self.environment.step()
+            # print(i, self.environment.agent.location)
+
+    def test_agent_path(self):
+        self.assertEqual(self.environment.agent.location, (40, 40))
+
+    def test_agent_grid(self):
+        self.assertIsInstance(
+            self.environment.grid.get_objects_from_grid('Sites',self.environment.agent.location)[0], Sites)
