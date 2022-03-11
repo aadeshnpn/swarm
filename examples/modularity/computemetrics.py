a540e53cf7eb2bbb7db7e1d443274e9f1bc2af96
Aadesh <aadeshnpn@byu.edu>
2022-03-11 14:50:37 -0500

    all size metrics computed

--- examples/modularity/computemetrics.py
+++ examples/modularity/computemetrics.py
@@ -1,4 +1,7 @@
 from ponyge.representation.grammar import Grammar
+import re
+import math
+import copy
 
 
 def compute_MCC(fname):
@@ -16,8 +19,48 @@ def compute_MCC(fname):
     return mcc
 
 
-def average_RHS(fname):
-    pass
+def compute_shis(fname):
+    # For each production rule
+    shi1 = 0
+    shi2 = 0
+    with open(fname, 'r') as f:
+        productions = f.readlines()
+    for production in productions:
+        # production = f.readline()
+        if production.rstrip():
+            # print(production.split('|'))s
+            shi1 += 1 + len(re.findall(r'\|', production))
+            rhsdecision = len(production.split('|'))
+            shi2 += rhsdecision
+    return shi1, shi2
+
+
+def average_RHS(fname, nt):
+    sizes = 0
+    with open(fname, 'r') as f:
+        productions = f.readlines()
+    for i, production in enumerate(productions):
+        if production.rstrip():
+            rhs = production.split('::=')[1]
+            rhsm = rhs.replace('<', '(')
+            rhsm = rhsm.replace('>', ')')
+            sizes += len(re.findall(r'\(((?:\w+\s*)+)\)', rhsm))
+
+            all = rhs.split('|')
+            for a in all:
+                b = a.split('_')[0]
+                if b.isalpha():
+                    sizes += 1
+
+    return round(sizes/nt, 2)
+
+
+def compute_HAL(fname, T, N):
+    meu1 = 1
+    meu2 = T + N
+    shi1, shi2 = compute_shis(fname)
+    hal = (meu1 * shi2 * (shi1 + shi2) * math.log2(meu1 + meu2)) / (2 * meu2)
+    return round(hal, 2)
 
 
 def load_grammar(gname='/tmp/coevo.bnf'):
@@ -33,12 +76,16 @@ def load_grammar(gname='/tmp/coevo.bnf'):
     parameter.params['POPULATION_SIZE'] = 10
     parameter.set_params(parameter_list)
     bnf = Grammar(parameter, gname)
+    T = len(set(list(bnf.terminals.keys())))
+    N = len(set(list(bnf.non_terminals.keys())))
     print(
-        'terminals: %i, non-terminals: %i, mcc: %i ' % (
-        len(bnf.terminals), len(bnf.non_terminals), compute_MCC(gname)))
+        'terminals: %i, non-terminals: %i, mcc: %i, arhs: %0.2f, hal: %0.2f ' %
+        (T, N, compute_MCC(gname), average_RHS(gname, N), compute_HAL(gname, T, N)))
 
+    # print(compute_HAL(gname, T, N))
     # print(dir(bnf))
     # print(compute_MCC(gname))
+    # print(average_RHS(gname, len(bnf.non_terminals)))
 
 
 def main():
