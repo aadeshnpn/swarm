099c413429f4a54074fe1e71999c526b1ae0cd5b
Aadesh <aadeshnpn@byu.edu>
2022-03-18 16:46:02 -0400

    power and eficciency computer for ablation pertrubation for foraging task

--- examples/coevolution/resilience_metrics.py
+++ examples/coevolution/resilience_metrics.py
@@ -3,6 +3,13 @@
 import os
 import numpy as np
 import pathlib
+from sklearn.linear_model import LinearRegression
+import matplotlib
+# If there is $DISPLAY, display the plot
+if os.name == 'posix' and "DISPLAY" not in os.environ:
+    matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt     # noqa: E402
 
 
 def read_data_n_agent_perturbations_all(
@@ -45,7 +52,7 @@ def read_data_n_agent_perturbations_all(
 
 def ablation_efficiency_power_foraging(t=7, time=1000):
     no_obstacles = [1, 2, 3, 4, 5]
-    times = list(range(1000, 5001, 1000))
+    times = list(range(1000, 12001, 1000))
     baseline = read_data_n_agent_perturbations_all(
         n=100, iter=12000, threshold=t, time=10000, iprob=0.85,
         addobject='None', radius=5, idx=[2])
@@ -62,8 +69,11 @@ def ablation_efficiency_power_foraging(t=7, time=1000):
                 len(effbaseline100))])]
     effbaseline100 = [(((12000-effbaseline100[i])/12000)*100) for i in range(len(effbaseline100))]
     # print(powerbaseline100, effbaseline100)
-    allpower = {0: powerbaseline100}
-    allefficiency = {0: effbaseline100}
+    # allpower = {0: [np.median(powerbaseline100)]}
+    # allefficiency = {0: [np.median(effbaseline100)]}
+    allpower = {}
+    allefficiency = {}
+
     for o in no_obstacles:
         data = [read_data_n_agent_perturbations_all(
             n=100, iter=12000, threshold=t, time=times[j], iprob=0.85,
@@ -73,23 +83,54 @@ def ablation_efficiency_power_foraging(t=7, time=1000):
         powerdata100 = [np.squeeze(
             data[i])[:, -1] for i in range(
                 len(times)) if data[i] is not None]
+        # powerdata100 = [np.median(powerdata100[i]) for i in range(len(powerdata100))]
+        # allpower[o] = powerdata100
         allpower[o] = np.concatenate(powerdata100, axis=0)
-        # powerdata100 = [powerbaseline100] + powerdata100
-        # print('power', powerdata100)
+
         effdata100 = []
-        for t in range(len(times)):
+        for ti in range(len(times)):
             # print(o, t)
-            if data[t] is not None:
+            if data[ti] is not None:
                 # print(o, data[t].shape)
-                effdata = np.squeeze(data[t])
+                effdata = np.squeeze(data[ti])
                 effdata = [np.squeeze(np.argwhere(effdata[i, :]>=80)) for i in range(effdata.shape[0])]
                 effdata = [np.array([effdata[i][0] if effdata[i].shape[0]>1 else 12000 for i in range(len(effdata))])]
                 effdata = [(((12000-effdata[i])/12000)*100) for i in range(len(effdata))]
+                # effdata = [np.median(effdata[i]) for i in range(len(effdata))]
                 effdata100 += effdata
-            # print('eff', effdata100)
-        allefficiency[o] = np.round(np.concatenate(effdata100, axis=0), 2)
-    # print(len(allpower[1]))
-    # print(allpower[1], allefficiency[1])
+        # allefficiency[o] = np.round(effdata100, 2)
+        # allefficiency[o] = np.round(np.array(effdata100), 2)
+        allefficiency[o] = np.round(np.concatenate(effdata100), 2)
+
+    print(allpower, allefficiency)
+    # linear_regressor = LinearRegression()  # create object for the class
+    X = np.concatenate(np.array([[k]* len(v) for k, v  in allpower.items()]))
+    Y = np.concatenate(np.array([v for _, v in allpower.items()]))
+    # X = X.reshape((X.shape[0], 1))
+    # print(X, Y)
+    # linear_regressor.fit(X, Y)  # perform linear regression
+    # Y_pred = linear_regressor.predict(X)  # make predictions
+    # print(linear_regressor.intercept_, linear_regressor.coef_)
+    par = np.polyfit(X, Y, 1, full=True)
+    m = par[0][0]
+    b = par[0][1]
+    print(m, b)
+    fig, ax = plt.subplots()
+    ax.scatter(X, Y)
+    ax.plot(X, m*X + b)
+    plt.show()
+
+    X = np.concatenate(np.array([[k]* len(v) for k, v  in allefficiency.items()]))
+    Y = np.concatenate(np.array([v for _, v in allefficiency.items()]))
+    par = np.polyfit(X, Y, 1, full=True)
+    m = par[0][0]
+    b = par[0][1]
+    print(m, b)
+    fig, ax = plt.subplots()
+    ax.scatter(X, Y)
+    ax.plot(X, m*X + b)
+    plt.show()
+    np.save('/tmp/foraging_power_efficiency_ablation.npy', np.array([allpower, allefficiency], dtype=object))
 
 
 def main():
