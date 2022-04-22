302058fa03b9f8d8545ce2c466057e1154ba8794
Aadesh <aadeshnpn@byu.edu>
2022-04-22 11:11:07 -0400

    ants conference project example

---
+++ examples/ants22/agent.py
@@ -0,0 +1,446 @@
+from inspect import CO_ITERABLE_COROUTINE
+from py_trees import common
+from py_trees.composites import Selector, Sequence, Parallel
+from py_trees import common, blackboard
+from py_trees.trees import BehaviourTree
+from swarms.behaviors.sbehaviors import DropCue, SendSignal, ObjectsStore
+import numpy as np
+from swarms.lib.agent import Agent
+from swarms.utils.bt import BTConstruct
+from swarms.utils.results import Results    # noqa : F401
+
+from ponyge.operators.initialisation import initialisation
+from ponyge.fitness.evaluation import evaluate_fitness
+from ponyge.operators.crossover import crossover
+from ponyge.operators.mutation import mutation
+from ponyge.operators.replacement import replacement
+from ponyge.operators.selection import selection
+
+import py_trees
+import copy
+from flloat.parser.ltlf import LTLfParser
+
+
+class CoevoAgent(Agent):
+    """An minimalistic Coevolution swarm agent."""
+
+    def __init__(self, name, model):
+        """Initialize the agent."""
+        super().__init__(name, model)
+        self.location = ()
+        self.prev_location = ()
+        # Agent attributes for motion
+        self.direction = model.random.rand() * (2 * np.pi)
+        self.speed = 2
+        self.radius = 3
+        self.shared_content = dict()
+        self.food_collected = 0
+        # Results
+        self.results = "db"  # This can take 2 values. db or file
+        # Behavior Tree Class
+        self.bt = BTConstruct(None, self)
+        # Location history
+        self.location_history = set()
+
+        # Communication history
+        self.signal_time = 0
+        self.no_cue_dropped = 0
+        # Step count
+        self.timestamp = 0
+        self.step_count = 0
+
+        self.fitness_name = True
+        self.ltrate = 0     # LT flag
+        self.geneticrate = False
+
+    def init_evolution_algo(self):
+        """Agent's GE algorithm operation defination."""
+        # This is a abstract class. Only the agents
+        # with evolving nature require this
+        pass
+
+    def construct_bt(self):
+        """Abstract method to construct BT."""
+        # Different agents has different way to construct the BT
+        pass
+
+    # New Agent methods for behavior based robotics
+    def sense(self):
+        """Sense included in behavior tree."""
+        pass
+
+    def plan(self):
+        """Plan not required for now."""
+        pass
+
+    def step(self):
+        """Agent action at a single time step."""
+        # Abstract class only evoling agent need this class
+        pass
+
+    def advance(self):
+        """Require for staged activation."""
+        pass
+
+    def get_food_in_hub(self, agent_name=True):
+        """Get the food in the hub stored by the agent."""
+        grid = self.model.grid
+        hub_loc = self.model.hub.location
+        neighbours = grid.get_neighborhood(hub_loc, self.model.hub.radius)
+        food_objects = grid.get_objects_from_list_of_grid('Food', neighbours)
+        agent_food_objects = []
+        if not agent_name:
+            for food in food_objects:
+                agent_food_objects.append(food)
+        else:
+            for food in food_objects:
+                if food.agent_name == self.name:
+                    agent_food_objects.append(food)
+        return agent_food_objects
+
+    def get_debris_transported(self):
+        """Return debris that have been cleared from hub."""
+        # Not computational efficient method
+        debris_objects = []
+        grid = self.model.grid
+        for boundary in self.model.boundaries:
+            boundary_loc = boundary.location
+            neighbours = grid.get_neighborhood(boundary_loc, boundary.radius)
+            debris_objects += grid.get_objects_from_list_of_grid('Debris', neighbours)
+        debris_objects = set(debris_objects)
+
+        agent_debris_objects = []
+        for debris in debris_objects:
+            try:
+                if (
+                    debris.agent_name == self.name and
+                        debris.phenotype == self.individual[0].phenotype):
+                    agent_debris_objects.append(debris)
+            except AttributeError:
+                pass
+        return agent_debris_objects
+
+    def detect_food_carrying(self):
+        """Check if the agent is carrying food."""
+        if len(self.attached_objects) > 0:
+            print('Food carying', self.name, self.attached_objects)
+            output = py_trees.display.ascii_tree(self.bt.behaviour_tree.root)
+            print(output)
+
+    def carrying_fitness(self):
+        """Compute carrying fitness.
+
+        This fitness supports the carrying behavior of
+        the agents.
+        """
+        # return len(self.attached_objects) * (self.timestamp)
+        return sum([obj.weight for obj in self.attached_objects])
+
+    def exploration_fitness(self):
+        """Compute the exploration fitness."""
+        # Use exploration space as fitness values
+        locations = len(self.location_history)
+        if locations == 0:
+            return 0
+        else:
+            return locations - 1
+
+    def communication_fitness(self):
+        """Compute communication fitness. """
+
+        childrens = list(self.bt.behaviour_tree.root.iterate())
+        for child in childrens:
+            if isinstance(child, SendSignal) and child.status == py_trees.Status.SUCCESS:
+                self.signal_time += 1
+            if isinstance(child, DropCue) and child.status == py_trees.Status.SUCCESS:
+                self.no_cue_dropped += 1
+        return self.signal_time + self.no_cue_dropped
+
+    """Function related to LTLf and goals."""
+    def evaluate_trace(self, goalspec, trace):
+        # Evaluate the trace
+        parser = LTLfParser()
+        parsed_formula = parser(goalspec)
+        result = parsed_formula.truth(trace)
+        return result
+
+
+class LearningAgent(CoevoAgent):
+    """Simple agent with GE capabilities."""
+
+    def __init__(self, name, model, threshold=10):
+        """Initialize the agent."""
+        super().__init__(name, model)
+        self.delayed_reward = 0
+        self.phenotypes = dict()
+
+        # Flags to make the computation easier
+        self.avoid_trap = False
+        self.avoid_obs = False
+        self.blackboard = blackboard.Client(name=str(self.name))
+        self.blackboard.register_key(key='neighbourobj', access=common.Access.WRITE)
+        self.blackboard.neighbourobj = dict()
+
+        self.selectors_reward = 0
+        self.constraints_reward = 0
+        self.postcond_reward = 0
+        self.threshold = threshold
+        # self.trace.append({k:self.functions[k]() for k in self.keys})
+        # self.trace[self.step_count] = {k:self.functions[k]() for k in self.keys}
+
+    def evaluate_constraints_conditions(self):
+        allnodes = list(self.bt.behaviour_tree.root.iterate())
+        selectors = list(filter(
+            lambda x: isinstance(x, Selector), allnodes)
+            )
+
+        constraints = list(filter(
+            lambda x: x.name.split('_')[-1] == 'constraint', allnodes)
+            )
+
+        postcond = list(filter(
+            lambda x: x.name.split('_')[-1] == 'postcond', allnodes)
+            )
+        # print(list(self.bt.behaviour_tree.visitors))
+        self.selectors_reward = sum([1 for sel in selectors if sel.status == common.Status.SUCCESS])
+        self.constraints_reward = sum([-2 for const in constraints if const.status == common.Status.FAILURE])
+        self.postcond_reward = sum([1 for pcond in postcond if pcond.status == common.Status.SUCCESS])
+        return self.selectors_reward + self.constraints_reward + self.postcond_reward
+
+    def init_evolution_algo(self):
+        """Agent's GE algorithm operation defination."""
+        # Genetic algorithm parameters
+        self.operation_threshold = 50
+        self.genome_storage = []
+
+        # Grammatical Evolution part
+        from ponyge.algorithm.parameters import Parameters
+        parameter = Parameters()
+        parameter_list = ['--parameters', '../..,ants.txt']
+        # Comment when different results is desired.
+        # Else set this for testing purpose
+        # parameter.params['RANDOM_SEED'] = name
+        # # np.random.randint(1, 99999999)
+        # Set GE runtime parameters
+        parameter.params['POPULATION_SIZE'] = self.operation_threshold // 2
+        parameter.set_params(parameter_list)
+        self.parameter = parameter
+        # Initialize the genome
+        individual = initialisation(self.parameter, size=3)
+        individual = evaluate_fitness(individual, self.parameter)
+        # Assign the genome to the agent
+        self.individual = individual
+        # Fitness
+        self.beta = 0.9
+        self.diversity_fitness = self.individual[0].fitness
+        self.individual[0].fitness = 0
+        self.generation = 0
+
+        self.delayed_cf = 0
+        self.delayed_ef = 0
+
+    def construct_bt(self):
+        """Construct BT."""
+        # Get the phenotype of the genome and store as xmlstring
+        self.bt.xmlstring = self.individual[0].phenotype
+        # Construct actual BT from xmlstring
+        self.bt.construct()
+        # print(self.bt.xmlstring)
+
+    def store_genome(self, cellmates):
+        """Store the genome from neighbours."""
+        # cellmates.remove(self)
+        self.genome_storage += [agent.individual[0] for agent in cellmates]
+
+    def exchange_chromosome(self,):
+        """Perform genetic operations."""
+        # print('from exchange', self.name)
+        individuals = self.genome_storage
+        parents = selection(self.parameter, individuals)
+        cross_pop = crossover(self.parameter, parents)
+        new_pop = mutation(self.parameter, cross_pop)
+        new_pop = evaluate_fitness(new_pop, self.parameter)
+        individuals = replacement(self.parameter, new_pop, individuals)
+        individuals.sort(reverse=True)
+        self.individual = [individuals[0]]
+        self.individual[0].fitness = 0
+        self.genome_storage = []
+
+    def genetic_step(self):
+        """Additional procedures called after genetic step."""
+
+        self.delayed_reward = self.individual[0].fitness
+        self.exchange_chromosome()
+        self.bt.xmlstring = self.individual[0].phenotype
+        self.bt.construct()
+        # Reset the variabes
+        self.food_collected = 0
+        self.location_history = set()
+        self.no_cue_dropped = 0
+        self.signal_time = 0
+        self.signals = []
+        self.timestamp = 0
+        self.diversity_fitness = self.individual[0].fitness
+        self.generation += 1
+
+    def overall_fitness(self):
+        """Compute complete fitness.
+
+        Goals are represented by objective function. We use combination of
+        objective function to define overall fitness of the agents
+        performance.
+        """
+        # Use a decyaing function to generate fitness
+        # Use two step decaying function
+        # First block gives importance to exploration and when as soon
+        # food has been found, the next block will focus on dropping
+        # the food on hub
+
+        # self.delayed_reward = round(self.beta * self.delayed_reward, 4)
+
+        # # Goal Specification Fitness
+        self.individual[0].fitness = (1-self.beta) * self.delayed_reward + self.ef + self.evaluate_constraints_conditions()
+
+    def get_food_in_hub(self, agent_name=True):
+        """Get the food in the hub stored by the agent."""
+        # grid = self.model.grid
+        # hub_loc = self.model.hub.location
+        # neighbours = grid.get_neighborhood(hub_loc, self.model.hub.radius)
+        # food_objects = grid.get_objects_from_list_of_grid('Food', neighbours)
+        agent_food_objects = []
+        food_objects = list(set(self.hub.dropped_objects))
+        if not agent_name:
+            for food in food_objects:
+                agent_food_objects.append(food.weight)
+        else:
+            for food in food_objects:
+                # print('p keys', self.name, food.phenotype.keys())
+                if (
+                    food.agent_name == self.name and (
+                        self.individual[0].phenotype in list(
+                            food.phenotype.keys())
+                        )):
+                    agent_food_objects.append(food.weight)
+        return sum(agent_food_objects)
+
+    def step(self):
+        """Take a step in the simulation."""
+        # py_trees.logging.level = py_trees.logging.Level.DEBUG
+        # output = py_trees.display.ascii_tree(self.bt.behaviour_tree.root)
+
+        # Couting variables
+        self.timestamp += 1
+        self.step_count += 1
+        self.prev_location = copy.copy(self.location)
+        self.geneticrate = False
+        self.ltrate = 0
+        # Increase beta
+        # self.beta = self.timestamp / self.model.iter
+        # if self.name ==1:
+        # print(self.timestamp, self.name, len(self.trace))
+        if self.dead is False:
+            # Compute the behavior tree
+            self.bt.behaviour_tree.tick()
+
+            # Maintain location history
+            _, gridval = self.model.grid.find_grid(self.location)
+            self.location_history.add(gridval)
+
+            # Hash the phenotype with its fitness
+            # We need to move this from here to genetic step
+            self.ef = self.exploration_fitness()
+
+            # Computes overall fitness using Beta function
+            self.overall_fitness()
+            # print(self.name, self.individual[0].fitness)
+
+            self.phenotypes = dict()
+            self.phenotypes[self.individual[0].phenotype] = (
+                self.individual[0].fitness)
+
+            if not self.model.stop_lateral_transfer:
+                # Find the nearby agents
+                cellmates = self.model.grid.get_objects_from_grid(
+                    type(self).__name__, self.location)
+
+                # Interaction Probability with other agents
+                cellmates = [cell for cell in cellmates  if self.model.random.rand() < self.model.iprob and cell.dead is False]
+                # cellmates = [cell for cell in cellmates if cell.individual[0] not in self.genome_storage]
+                self.ltrate = len(cellmates)
+                # If neighbours found, store the genome
+                if len(cellmates) > 1:
+                    # cellmates = list(self.model.random.choice(
+                    #     cellmates, self.model.random.randint(
+                    #         1, len(cellmates)-1), replace=False))
+                    self.store_genome(cellmates)
+
+            # Logic for gentic operations.
+            # If the genome storage has enough genomes and agents has done some
+            # exploration then compute the genetic step OR
+            # 200 time step has passed and the agent has not done anything useful
+            # then also perform genetic step
+            storage_threshold = len(
+                self.genome_storage) >= self.threshold
+            # (self.model.num_agents / (self.threshold* 1.0))
+
+            if storage_threshold:
+                self.geneticrate = storage_threshold
+                self.genetic_step()
+            elif (
+                    (
+                        storage_threshold is False and self.timestamp > self.model.gstep
+                        ) and (self.exploration_fitness() < self.model.expp)):
+                individual = initialisation(self.parameter, 10)
+                individual = evaluate_fitness(individual, self.parameter)
+                self.genome_storage = self.genome_storage + individual
+                self.genetic_step()
+
+
+class ExecutingAgent(CoevoAgent):
+    """A coevolution swarm agent.
+
+    This agent will run the various behaviors evolved.
+    """
+
+    def __init__(self, name, model, xmlstring=None):
+        """Initialize the agent."""
+        super().__init__(name, model)
+        self.xmlstring = xmlstring
+        self.blackboard = blackboard.Client(name=str(self.name))
+        self.blackboard.register_key(key='neighbourobj', access=common.Access.WRITE)
+        self.blackboard.neighbourobj = dict()
+
+    def construct_bt(self):
+        """Construct BT."""
+        # Get the phenotype of the genome and store as xmlstring
+        # self.bt.xmlstring = self.xmlstring
+        # # Construct actual BT from xmlstring
+        # self.bt.construct()
+
+        # New way to create BT
+        bts = []
+        for i in range(len(self.xmlstring)):
+            bt = BTConstruct(None, self, self.xmlstring[i])
+            bt.construct()
+            bts.append(bt.behaviour_tree.root)
+        # root = Selector('RootAll')
+        # root = Sequence('RootAll')
+        root = Parallel('RootAll')
+        self.model.random.shuffle(bts)
+        root.add_children(bts)
+        self.bt.behaviour_tree = BehaviourTree(root)
+        # py_trees.display.render_dot_tree(
+        #    self.bt.behaviour_tree.root, name='/tmp/' + str(self.name))
+        # py_trees.logging.level = py_trees.logging.Level.DEBUG
+        # print(self.name, py_trees.display.ascii_tree(self.bt.behaviour_tree.root))
+
+    def step(self):
+        """Agent action at a single time step."""
+        # Maintain the location history of the agent
+        # self.location_history.add(self.location)
+
+        # Compute the behavior tree
+        self.bt.behaviour_tree.tick()
+
+        # Find the no.of food collected from the BT execution
+        # self.food_collected = len(self.get_food_in_hub())
---
+++ examples/ants22/experiment.py
@@ -0,0 +1,191 @@
+"""Experiment script to run coevolution simulation."""
+
+import numpy as np
+# import pdb
+# import hashlib
+# import sys
+import argparse
+from model import EvolveModel
+from swarms.utils.jsonhandler import JsonPhenotypeData
+from swarms.utils.graph import Graph, GraphACC  # noqa : F401
+from joblib import Parallel, delayed    # noqa : F401
+from swarms.utils.results import SimulationResultsLt
+# import py_trees
+# Global variables for width and height
+width = 100
+height = 100
+
+UI = False
+
+
+def learning_phase(args):
+    """Learning Algorithm block."""
+    iteration = args.iter
+    no_agents = args.n
+    db = args.db
+    # early_stop =False
+    threshold = args.threshold
+    gstep = args.gstep
+    expp = args.expp
+    # Evolution environment
+    env = EvolveModel(
+        no_agents, width, height, 10, iter=iteration, db=db,
+        threshold=threshold, gstep=gstep, expp=expp, args=args)
+    env.build_environment_from_json()
+    env.create_agents()
+    # Validation Step parameter
+    # Run the validation test every these many steps
+    # validation_step = 11000
+
+    # Iterate and execute each step in the environment
+    # Take a step i number of step in evolution environment
+    # Take a 1000 step in validation environment sampling from the evolution
+    # Make the validation envronmnet same as the evolution environment
+    ltrateavg, ltratestd = env.compute_lt_rate()
+    results = SimulationResultsLt(
+        env.pname, env.connect, env.sn, env.stepcnt,
+        env.foraging_percent(), None, env.no_agent_dead(),
+        ltrateavg, ltratestd, env.compute_genetic_rate(), db=False
+        )
+    # Save the data in a result csv file
+    results.save_to_file()
+    # import py_trees
+    # for agent in env.agents:
+    #     print(
+    #         agent.name, py_trees.display.ascii_tree(
+    #             agent.bt.behaviour_tree.root))
+
+    for i in range(iteration):
+        # Take a step in evolution
+        env.step()
+        ltrateavg, ltratestd = env.compute_lt_rate()
+        results = SimulationResultsLt(
+            env.pname, env.connect, env.sn, env.stepcnt,
+            env.foraging_percent(), None, env.no_agent_dead(),
+            ltrateavg, ltratestd, env.compute_genetic_rate(), db=False
+            )
+        # Save the data in a result csv file
+        results.save_to_file()
+        # env.gather_info()
+
+    # Update the experiment table
+    foraging_percent = env.foraging_percent()
+    if foraging_percent > 5:
+        success = True
+    else:
+        success = False
+    env.experiment.update_experiment_simulation(foraging_percent, success)
+
+    # print('max, min generations', np.max(generations), np.min(generations))
+    # pdb.set_trace()
+    # allphenotypes = env.behavior_sampling_objects(ratio_value=1.0)
+    # # save the phenotype to json file
+    # phenotype_to_json(
+    #     env.pname, env.runid + '-' + 'all', allphenotypes)
+
+    # print('total generation', [agent.generation for agent in env.agents])
+    # try:
+    #     # return list(phenotypes.keys())
+    #     return allphenotypes, foraging_percent, env.pname
+    # except UnboundLocalError:
+    #     return None, None, None
+
+
+def phenotype_to_json(pname, runid, phenotypes):
+    """Store the phenotype to a json file."""
+    jfilename = pname + '/' + runid + '.json'
+    JsonPhenotypeData.to_json(phenotypes, jfilename)
+
+
+def exp_evol(iter, n, db):
+    """Block for the main function."""
+    # Run the evolutionary learning algorithm
+    phenotypes = learning_phase(iter, n, db)    # noqa : F841
+    # learning_phase(iter)
+    # Run the evolved behaviors on a test environment
+    # if phenotypes is not None:
+    #     test_loop(phenotypes, 5000)
+
+
+def standard_evolution(args):
+    # phenotypes = learning_phase(iter, n, db)
+    if args.threads <= 1:
+        for i in range(args.runs):
+            learning_phase(args)
+    else:
+        Parallel(
+                n_jobs=args.threads)(
+                    delayed(learning_phase)(args) for i in range(
+                        args.runs))
+
+
+def experiments(args):
+    ## New experiments      # noqa : E266
+    ## Remove the communication behavior nodes  # noqa : E266
+    ## Increase the size of the traps and obstacles     # noqa : E266
+    ## Increase the size of the world   # noqa : E266
+    exp_no = {
+        0: standard_evolution,
+        1: exp_varying_n_evolution,
+        # 2: behavior_sampling,
+        # 3: single_evo,
+        # 4: behavior_sampling_after,
+        # 5: exp_with_size_trap
+    }
+    exp_no[args.exp_no](args)
+
+
+def exp_varying_n_evolution(args):
+    for n in [100, 150, 200]:
+        Parallel(
+            n_jobs=args.threads)(
+                delayed(
+                    exp_evol)(args.iter, n, args.db) for i in range(args.runs))
+    # for i in range(args.runs):
+    #     exp_evol_sample(args.iter, 100, args.db)
+
+
+# def behavior_sampling(args):
+#     exp_evol_sample(args.iter, 50, args.db, args.runs, args.threads)
+
+
+def single_evo(args):
+    exp_evol(args.iter, 50, False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--exp_no', default=0, type=int)
+    parser.add_argument('--runs', default=1, type=int)
+    parser.add_argument('--threads', default=1, type=int)
+    parser.add_argument('--iter', default=12000, type=int)
+    parser.add_argument('--db', default=False, type=bool)
+    parser.add_argument('--threshold', default=10, type=int)
+    parser.add_argument('--gstep', default=200, type=int)
+    parser.add_argument('--expp', default=2, type=int)
+    parser.add_argument('--n', default=50, type=int)
+    parser.add_argument(
+        '--addobject', default=None, choices=[
+            None, 'Obstacles', 'Traps', 'Hub', 'Sites'], type=str)
+    parser.add_argument(
+        '--removeobject', default=None, choices=[
+            None, 'Obstacles', 'Traps', 'Hub', 'Sites'], type=str)
+    parser.add_argument(
+        '--moveobject', default=None, choices=[
+            None, 'Obstacles', 'Traps', 'Hub', 'Sites'], type=str)
+    parser.add_argument(
+        '--jamcommun', default=None, choices=[None, 'Cue', 'Signal'], type=str)
+    parser.add_argument('--probability', default=0.5, type=float)
+    parser.add_argument('--no_objects', default=1, type=int)
+    parser.add_argument('--location', default=(-np.inf, -np.inf), type=str)
+    parser.add_argument('--radius', default=5, type=int)
+    parser.add_argument('--time', default=10000, type=int)
+    parser.add_argument('--stoplen', default=0, type=int)
+    parser.add_argument('--iprob', default=0.85, type=float)
+    parser.add_argument('--stop_interval', default=1000, type=int)
+    parser.add_argument('--no_debris', default=10, type=int)
+    args = parser.parse_args()
+    args.stoplen = args.time + args.stop_interval
+    print(args)
+    experiments(args)
